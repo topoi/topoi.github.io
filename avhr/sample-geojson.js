ad2faac947a77fb8da4a02f10aa434e873f38686
grobian <gordon-fischer@gmx.de>
2020-11-18 15:10:35 +0100

    Add files via upload

---
+++ avhr/sample-geojson.js
@@ -0,0 +1,52 @@
+
+var Russland_Ehrenberg = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Ehrenberg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [40.98999178409576, 35.74683938093904]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Ehrenberg 2"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [35.98689115047453, 32.747924136466565]
+            }
+        }
+    ]
+};
+
+var Russland_Humboldt = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Humboldt"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [32.98999178409576, 35.74683938093904]
+            }
+        },{
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Humboldt"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [37.98689115047453, 32.747924136466565]
+            }
+        }
+    ]
+};
+
+
