97c994939f7def97c16c0e3c77c815a95d5d71ce
grobian <gordon-fischer@gmx.de>
2020-11-19 13:28:00 +0100

    Add files via upload

--- avhr/sample-geojson.js
+++ avhr/sample-geojson.js
@@ -5,48 +5,22 @@ var Russland_Ehrenberg = {
         {
             "type": "Feature",
             "properties": {
-                "popupContent": "Ehrenberg"
+		"key": "H0002298",
+		
+                "ContentHeader": "Ort XY",
+		
+		"Content": {"1":{"folio": "12r", "date": "1829-04-21", "text": "Sind gestern in XY angekommen. Wollen 10 Tage bleiben", "url":"https://edition-humboldt.de/reisetagebuecher/detail.xql?id=H0018407&l=de&view=f&page=12r"},
+			    "2":{"folio": "13r", "date": "1829-04-23", "text": "Ehrenberg geht mir auf die Nerven, ich werde in in XY zurücklassen.", "url":"https://edition-humboldt.de/reisetagebuecher/detail.xql?id=H0018407&l=de&view=f&page=13r"},
+			    "3":{"folio": "26r", "date": "1829-07-02", "text": "plane demnächst nach Tanzania aufzubrechen, kann auch nicht schlimmer sein als XY", "url":"https://edition-humboldt.de/reisetagebuecher/detail.xql?id=H0018407&l=de&view=f&page=26r"}
+			    }
             },
             "geometry": {
                 "type": "Point",
                 "coordinates": [40.98999178409576, 35.74683938093904]
             }
-        },{
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Ehrenberg 2"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [35.98689115047453, 32.747924136466565]
-            }
         }
     ]
 };
 
-var Russland_Humboldt = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Humboldt"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [32.98999178409576, 35.74683938093904]
-            }
-        },{
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Humboldt"
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [37.98689115047453, 32.747924136466565]
-            }
-        }
-    ]
-};
 
 
