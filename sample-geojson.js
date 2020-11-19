1982b473f97af9fbe101afd8dfaf017cd9527088
grobian <gordon-fischer@gmx.de>
2020-11-19 13:27:13 +0100

    Add files via upload

---
+++ sample-geojson.js
@@ -0,0 +1,26 @@
+
+var Russland_Ehrenberg = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+		"key": "H0002298",
+		
+                "ContentHeader": "Ort XY",
+		
+		"Content": {"1":{"folio": "12r", "date": "1829-04-21", "text": "Sind gestern in XY angekommen. Wollen 10 Tage bleiben", "url":"https://edition-humboldt.de/reisetagebuecher/detail.xql?id=H0018407&l=de&view=f&page=12r"},
+			    "2":{"folio": "13r", "date": "1829-04-23", "text": "Ehrenberg geht mir auf die Nerven, ich werde in in XY zurücklassen.", "url":"https://edition-humboldt.de/reisetagebuecher/detail.xql?id=H0018407&l=de&view=f&page=13r"},
+			    "3":{"folio": "26r", "date": "1829-07-02", "text": "plane demnächst nach Tanzania aufzubrechen, kann auch nicht schlimmer sein als XY", "url":"https://edition-humboldt.de/reisetagebuecher/detail.xql?id=H0018407&l=de&view=f&page=26r"}
+			    }
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [40.98999178409576, 35.74683938093904]
+            }
+        }
+    ]
+};
+
+
+
